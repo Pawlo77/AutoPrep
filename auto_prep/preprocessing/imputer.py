68c2efee3d195fda221caf2a5f9cd885756335ee
krukj <126516292+krukj@users.noreply.github.com>
2024-12-20 22:15:02 +0100

    add basic encoder and imputer

--- auto_prep/preprocessing/imputer.py
+++ auto_prep/preprocessing/imputer.py
@@ -1,69 +1,188 @@
 import pandas as pd
-
-from ..utils.abstract import Numerical, RequiredStep
-from ..utils.logging_config import setup_logger
+from sklearn.impute import SimpleImputer
+from utils.logging_config import setup_logger
 
 logger = setup_logger(__name__)
 
 
-class NumericalDataImputer(RequiredStep, Numerical):
+class NAImputer:
     """
-    Performs Numerical data imputation
+    Base class for imputing missing values. Provides functionality
+    to identify columns with missing values and determine the strategy to handle them
+    (remove columns with >50% missing data).
+
+    Attributes:
+        numeric_features (list): A list of numeric feature names.
+        categorical_features (list): A list of categorical feature names.
     """
 
-    param_grid = {
-        "strategy": "median",
-    }
+    def __init__(self):
+        self.numeric_features = []
+        self.categorical_features = []
 
-    def __init__(self, strategy="median") -> None:
-        self.strategy = strategy
-
-    def fit(self, X: pd.DataFrame, y=None) -> "NumericalDataImputer":
-        """Identify feature types in the dataset.
+    def fit(self, X: pd.DataFrame) -> "NAImputer":
+        """
+        Identifies columns with more than 50% missing values and removes them
+        from the dataset.
 
         Args:
-            X (pd.DataFrame): Input features.
-            y: Ignored. Exists for scikit-learn compatibility.
+            X (pd.DataFrame): The input data with missing values.
 
         Returns:
-            NumericalDataImputer: Fitted transformer.
-        Raises:
-            ValueError if non numerical column included in X.
+            NAImputer: The fitted imputer instance.
         """
+        logger.start_operation(
+            f"Fitting NAImputer to data with {X.shape[0]} rows and {X.shape[1]} columns."
+        )
 
-        logger.start_operation(f"Numerical data fit ({X.shape[1]} columns).")
-        for column in X.columns:
-            if not pd.api.types.is_numeric_dtype(X[column]):
-                raise ValueError(f"Non numerical feature found: {column}.")
+        # Removing columns with >50% missing values
+        missing_threshold = 0.5
+        cols_to_remove = [
+            col for col in X.columns if X[col].isnull().mean() > missing_threshold
+        ]
+        logger.debug(
+            f"Columns to be removed due to >50% missing values: {cols_to_remove}"
+        )
+        # Update internal state but do not modify input DataFrame
+        self.cols_to_remove = cols_to_remove
+
         logger.end_operation()
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Removes previously identified columns with >50% missing values.
+
+        Args:
+            X (pd.DataFrame): The input data to transform.
+
+        Returns:
+            pd.DataFrame: The transformed data.
+        """
+        return X.drop(columns=self.cols_to_remove, errors="ignore")
+
+
+class NumericalImputer(NAImputer):
+    """
+    Imputer for numerical columns. This class fills missing values in numerical columns
+    with the median of the respective column.
+
+    Attributes:
+        strategy (str): The imputation strategy (default: "median").
+        imputer (SimpleImputer): The SimpleImputer instance for filling missing values.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.strategy = "median"
+        self.imputer = SimpleImputer(strategy=self.strategy)
+
+    def fit(self, X: pd.DataFrame) -> "NumericalImputer":
+        """
+        Identifies numeric columns and fits the imputer.
+
+        Args:
+            X (pd.DataFrame): The input data.
+
+        Returns:
+            NumericalImputer: The fitted imputer instance.
+        """
+        super().fit(X)
+        self.numeric_features = [
+            col for col in X.columns if pd.api.types.is_numeric_dtype(X[col])
+        ]
+
+        logger.debug(f"Identified numeric features: {self.numeric_features}")
+        self.imputer.fit(X[self.numeric_features])
 
         return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
-        Applies cleaning and transformation operations to the input data.
+        Fills missing values in numeric columns using the median strategy.
 
         Args:
-            X (pd.DataFrame): The input DataFrame to be cleaned and transformed.
+            X (pd.DataFrame): The input data to transform.
 
         Returns:
-            pd.DataFrame: The cleaned and transformed DataFrame.
+            pd.DataFrame: The transformed data with missing numeric values imputed.
         """
-        try:
-            X = X.copy()
+        X = super().transform(X)
+        X[self.numeric_features] = self.imputer.transform(X[self.numeric_features])
+        return X
 
-            logger.start_operation(f"Numerical data transform (shape - {X.shape}).")
-            for col in self.numeric_features:
-                missing_count = X[col].isnull().sum()
-                if missing_count > 0:
-                    logger.debug(
-                        f"Imputing {missing_count} missing values in {col} with median."
-                    )
-                    X[col] = X[col].fillna(X[col].median())
-            logger.end_operation()
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fits and transforms the input data by imputing missing values in numeric columns.
 
-            return X
+        Args:
+            X (pd.DataFrame): The input data.
 
-        except Exception as e:
-            logger.error(f"Data cleaning failed: {e}")
-            raise e
+        Returns:
+            pd.DataFrame: The transformed data with missing numeric values imputed.
+        """
+        return self.fit(X).transform(X)
+
+
+class CategoricalImputer(NAImputer):
+    """
+    Imputer for categorical columns. This class fills missing values in categorical columns
+    with the most frequent value in the respective column.
+
+    Attributes:
+        strategy (str): The imputation strategy (default: "most_frequent").
+        imputer (SimpleImputer): The SimpleImputer instance for filling missing values.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.strategy = "most_frequent"
+        self.imputer = SimpleImputer(strategy=self.strategy)
+
+    def fit(self, X: pd.DataFrame) -> "CategoricalImputer":
+        """
+        Identifies categorical columns and fits the imputer.
+
+        Args:
+            X (pd.DataFrame): The input data.
+
+        Returns:
+            CategoricalImputer: The fitted imputer instance.
+        """
+        super().fit(X)
+        self.categorical_features = [
+            col for col in X.columns if not pd.api.types.is_numeric_dtype(X[col])
+        ]
+
+        logger.debug(f"Identified categorical features: {self.categorical_features}")
+        self.imputer.fit(X[self.categorical_features])
+
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fills missing values in categorical columns using the most frequent value strategy.
+
+        Args:
+            X (pd.DataFrame): The input data to transform.
+
+        Returns:
+            pd.DataFrame: The transformed data with missing categorical values imputed.
+        """
+        X = super().transform(X)
+        X[self.categorical_features] = self.imputer.transform(
+            X[self.categorical_features]
+        )
+        return X
+
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fits and transforms the input data by imputing missing values in categorical columns.
+
+        Args:
+            X (pd.DataFrame): The input data.
+
+        Returns:
+            pd.DataFrame: The transformed data with missing categorical values imputed.
+        """
+        return self.fit(X).transform(X)
